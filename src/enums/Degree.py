004786e340024d7fc50d6807d1ba36a99f26e6e2
nicholasraynes <nicholasraynes@yahoo.com>
2026-02-06 14:35:37 -0500

    feat: Added CERTIFICATE as a degree option (for coding bootcamps or other relevant certificate issuing courses)

--- src/enums/Degree.py
+++ src/enums/Degree.py
@@ -3,9 +3,10 @@ from enum import Enum
 class Degree(Enum):
     NONE = 0
     DIPLOMA = 1
-    ASSOCIATES = 2
-    BACHELORS = 3
-    MASTERS = 4
-    DOCTORATE = 5
-    PHD = 6
+    CERTIFICATE = 2
+    ASSOCIATES = 3
+    BACHELORS = 4
+    MASTERS = 5
+    DOCTORATE = 6
+    PHD = 7
     
